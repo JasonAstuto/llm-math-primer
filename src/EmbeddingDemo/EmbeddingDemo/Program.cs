65ffcda91cba519cb1a8fe9b567c9399c81a45c0
Jason Astuto <Jason.Astuto@Gmail.com>
2025-06-13 04:59:22 -0700

    initial commit

---
+++ src/EmbeddingDemo/EmbeddingDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TorchSharp;
+using static TorchSharp.torch;
+using TorchSharp.Modules;
+
+class Program
+{
+    static void Main()
+    {
+        var vocab = new Dictionary<string, long>
+        {
+            { "the", 0 },
+            { "cat", 1 },
+            { "sat", 2 },
+            { "on", 3 },
+            { "mat", 4 }
+        };
+
+        string[] sentence = new[] { "the", "cat", "sat" };
+        long[] tokenIds = sentence.Select(word => vocab[word]).ToArray();
+
+        torch.random.manual_seed(42);
+        var device = torch.CPU;
+
+        var embedding = nn.Embedding(vocab.Count, 8).to(device);
+        var input = torch.tensor(tokenIds, dtype: ScalarType.Int64).to(device);
+        var vectors = embedding.forward(input);
+
+        Console.WriteLine("Token IDs: " + string.Join(", ", tokenIds));
+        Console.WriteLine("\nEmbeddings:");
+        for (int i = 0; i < vectors.shape[0]; i++)
+        {
+            var vector = vectors[i];
+            Console.Write($"{sentence[i],-5} → ");
+            Console.WriteLine(string.Join(", ", vector.data<float>().ToArray()));
+        }
+    }
+}
